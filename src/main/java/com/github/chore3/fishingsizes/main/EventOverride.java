d05a413c92c81d39c44be6384855f101de39b9eb
chore_3 <anon408chore@gmail.com>
2026-03-13 17:44:46 +0900

    feat: Loreにテキストを埋め込むプログラムを仮作成

--- src/main/java/com/github/chore3/fishingsizes/main/EventOverride.java
+++ src/main/java/com/github/chore3/fishingsizes/main/EventOverride.java
@@ -1,6 +1,11 @@
 package com.github.chore3.fishingsizes.main;
 
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.nbt.StringTag;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.Component.Serializer;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.event.entity.player.ItemFishedEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -10,6 +15,14 @@ public class EventOverride {
     @SubscribeEvent
     public static void onItemFished(ItemFishedEvent event) {
         if (!event.getEntity().level().isClientSide) {
+            for (ItemStack stack : event.getDrops()) {
+                if (!stack.isEmpty()) {
+                    CompoundTag displayTag = stack.getOrCreateTagElement("display");
+                    ListTag loreTag = new ListTag();
+                    loreTag.add(StringTag.valueOf(Serializer.toJson(Component.literal("test"))));
+                    displayTag.put("Lore", loreTag);
+                }
+            }
             event.getEntity().sendSystemMessage(Component.literal("釣りイベントが発生しました"));
         }
     }
